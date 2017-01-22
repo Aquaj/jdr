363b256f7b61c47ad24667fc41b9a2593c52bf00
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-22 18:06:49 +0100

    Added GM relation.

--- db/migrate/20170122131559_create_campaigns.rb
+++ db/migrate/20170122131559_create_campaigns.rb
@@ -2,7 +2,8 @@ class CreateCampaigns < ActiveRecord::Migration[5.0]
   def change
     create_table :campaigns do |t|
       t.string     :name
-      t.references :group
+      t.references :group, foreign_key: true
+      t.references :game_master
 
       t.timestamps
     end
