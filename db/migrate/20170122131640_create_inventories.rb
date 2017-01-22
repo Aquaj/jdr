0984fa814953e1d57c6d4b0d8c907a3063129ee4
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-22 17:26:23 +0100

    Removed blocking foreign_key on polymorphic association Inventory#owner.

--- db/migrate/20170122131640_create_inventories.rb
+++ db/migrate/20170122131640_create_inventories.rb
@@ -1,9 +1,9 @@
 class CreateInventories < ActiveRecord::Migration[5.0]
   def change
     create_table :inventories do |t|
-      t.references :owner, foreign_key: true
+      t.references :owner
 
       t.timestamps
     end
   end
-end
+end 
