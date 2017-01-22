7176c15c54af83c9189dcad5add9266fff0e80b6
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-22 12:08:08 +0100

    Added rbenv-ruby version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-set :rbenv_ruby_version, '2.4.0'
+set :rbenv_ruby, '2.4.0'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
