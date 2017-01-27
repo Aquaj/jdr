c19dabfa17038ccb2cff09a823829cfab1edaa07
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 20:49:42 +0100

    Added spec for Armours.

--- spec/factories/armours.rb
+++ spec/factories/armours.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
-  factory :armour do
-    
+  factory :plaid_shirt, class: :armour do
+    name           'Plaid shirt'
+    enc           4
+    covers        'Left arm'
+    armour_points 5
+    details       'Plaid'
   end
 end
