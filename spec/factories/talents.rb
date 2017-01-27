a945c42b06020b3bf8dc90f014de239ee594cbc7
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 21:53:33 +0100

    Added Talent factory.

--- spec/factories/talents.rb
+++ spec/factories/talents.rb
@@ -1,5 +1,21 @@
 FactoryGirl.define do
-  factory :talent do
-    
+  factory :strong_af, class: :talent do
+    name 'Strong AF'
+  end
+
+  factory :fabulous, class: :talent do
+    name 'Fabulous'
+  end
+
+  factory :gay, class: :talent do
+    name 'Gay'
+  end
+
+  factory :lethal, class: :talent do
+    name 'Lethal'
+  end
+
+  factory :secret, class: :talent do
+    name 'Secret'
   end
 end
