4ef4292508545ec1fb2bb40cb6510d78871ce68d
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 23:02:25 +0100

    Rewrote Skill Spec to make it cleaner.

--- spec/models/character_spec/skill_spec.rb
+++ spec/models/character_spec/skill_spec.rb
@@ -1,51 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe 'Character Skills', type: :model do
+  let :zendark { Character.find_by_name('Zendark') || create(:zendark) }
+
   describe '#gets_talent' do
+    let :strong   { Talent.find_by_name('Strong AF') || create(:strong_af) }
+    let :gay      { Talent.find_by_name('Gay')       || create(:gay) }
+    let :fabulous { Talent.find_by_name('Fabulous')  || create(:fabulous) }
+    let :lethal   { Talent.find_by_name('Lethal')    || create(:lethal) }
+    let :secret   { Talent.find_by_name('Secret')    || create(:secret_talent) }
+
     it 'adds a new talent to the char' do
-      zendark = create(:zendark)
-      zendark.gets_talent('Fabulous')
-      expect(zendark.talents)
-        .to eq(Talent.where(name: ['Strong AF', 'Gay', 'Fabulous']))
+      zendark.gets_talent fabulous
+      expect(zendark.talents).to eq [strong, gay, fabulous]
     end
 
     it 'errors when new talent is not available' do
-      expect { create(:zendark).gets_talent('Lethal') }
+      expect { zendark.gets_talent lethal }
         .to raise_error TalentUnavailableException
     end
 
     it 'errors when new talent is not known' do
-      expect { create(:zendark).gets_talent('Secret') }
+      expect { zendark.gets_talent secret }
         .to raise_error TalentUnknownException
-    end
-
-    it 'errors when new talent doesn\'t exist' do
-      expect { create(:zendark).gets_talent('NonExistent') }
-        .to raise_error NoTalentException
     end
   end
 
   describe '#gets_ability' do
+    let :dancing    { Ability.find_by_name('Dancing')       || create(:dancing) }
+    let :punching   { Ability.find_by_name('Punching')      || create(:punching) }
+    let :sassy_snap { Ability.find_by_name('Sassy Snap')    || create(:sassy_snap) }
+    let :death_snap { Ability.find_by_name('Snap of Death') || create(:snap_of_death) }
+    let :secret     { Ability.find_by_name('Secret')        || create(:secret_ability) }
+
     it 'adds a new ability to the char' do
-      zendark = create(:zendark)
-      zendark.gets_ability('Sassy Snap')
-      expect(zendark.abilities)
-        .to eq(Abilities.where(name: ['Dancing', 'Punching', 'Sassy Snap']))
+      zendark.gets_ability sassy_snap
+      expect(zendark.abilities).to eq [dancing, punching, sassy_snap]
     end
 
     it 'errors when new ability is not available' do
-      expect { create(:zendark).gets_ability('Snap of Death') }
+      expect { create(:zendark).gets_ability death_snap }
         .to raise_error AbilityUnavailableException
     end
 
     it 'errors when new ability is not known' do
-      expect { create(:zendark).gets_ability('Secret') }
+      expect { create(:zendark).gets_ability secret }
         .to raise_error AbilityUnknownException
-    end
-
-    it 'errors when new ability doesn\'t exist' do
-      expect { create(:zendark).gets_ability('NonExistent') }
-        .to raise_error NoAbilityException
     end
   end
 end
