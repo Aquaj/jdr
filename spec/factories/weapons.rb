57ae9862e37395f8910de9c4d5d43b49270deb6e
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 22:00:25 +0100

    Weapons factories. (**all factories done** time to refacto some.)

--- spec/factories/weapons.rb
+++ spec/factories/weapons.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
-  factory :weapon do
-    
+  factory :big_mace, class: :weapon do
+    name        'Big Mace'
+    enc         1
+    group       'Bludgeons'
+    damage      '1d+5'
+    reach       '1m'
+    reload      ''
+    details     'Big AF'
+    attributes  'Big'
   end
 end
