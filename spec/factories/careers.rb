3d58608a14defa248f62474067c4626259f7a5e1
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 21:25:47 +0100

    Added Career factory.

--- spec/factories/careers.rb
+++ spec/factories/careers.rb
@@ -1,5 +1,26 @@
 FactoryGirl.define do
-  factory :career do
-    
+  factory :farm_bull, class: :career do
+    name 'Farm Bull'
+  end
+
+  factory :diva, class: :career do
+    name 'Diva'
+  end
+
+  factory :icon, class: :career do
+    name 'Icon'
+  end
+
+  factory :secret, class: :career do
+    name   'Secret'
+    nature :hidden
+  end
+
+  factory :death_itself, class: :career do
+    name 'Death Itself'
+  end
+
+  factory :proplayer, class: :career do
+    name 'Proplayer'
   end
 end
