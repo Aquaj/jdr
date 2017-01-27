c9b0211641089b03f86149835153e5d95f8d5a9a
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 20:32:51 +0100

    Removal of StatBonusSets that were a useless 1:1 table

--- db/migrate/20170122132515_create_stat_bonus_sets.rb
+++
@@ -1,8 +0,0 @@
-class CreateStatBonusSets < ActiveRecord::Migration[5.0]
-  def change
-    create_table :stat_bonus_sets do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170122132530_create_stat_bonuses.rb
+++ db/migrate/20170122132530_create_stat_bonuses.rb
@@ -1,7 +1,7 @@
 class CreateStatBonuses < ActiveRecord::Migration[5.0]
   def change
     create_table :stat_bonuses do |t|
-      t.references :stat_bonus_set, foreign_key: true
+      t.references :origin
       t.integer    :amount
       t.string     :reason
       t.string     :context
