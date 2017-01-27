ad488d14cad19140516bb036f69a43b15f647bdb
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 20:50:00 +0100

    Added spec for Campaigns.

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
-  factory :campaign do
-    
+  factory :ultimate, class: :campaign do
+    name 'Ultimate Campaigning'
+    association :game_master, factory: :jeremie
+    association :group, factory: :the_losers
   end
 end
