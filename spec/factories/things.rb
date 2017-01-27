95c449dcdd054e6f683626c2cffca5270bf13215
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 21:57:20 +0100

    Completed Things factory.

--- spec/factories/things.rb
+++ spec/factories/things.rb
@@ -1,10 +1,17 @@
 FactoryGirl.define do
-  factory :thing do
+  factory :candles, class: :candles do
+    name          'Candles'  
+    unit          ''
+    details       'Scented'
+    quantity      '5'
+    enc_per_unit  1
+  end
+
+  factory :burger_king, class: :thing do
     name          'Burger King'  
     unit          'bks'
     details       'Wrapped'
     quantity      '15'
     enc_per_unit  1
-    localization  'In the bag'
   end
 end
