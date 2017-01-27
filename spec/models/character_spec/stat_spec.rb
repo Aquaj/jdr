ac9057c7e654004cc00e594ca948ed40a13c53ad
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 23:05:52 +0100

    Rewrote Stat Spec to make it cleaner.

--- spec/models/character_spec/stat_spec.rb
+++ spec/models/character_spec/stat_spec.rb
@@ -1,31 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Character Stats', type: :model do
-  subject(:character) { create(:character) }
-  it { should respond_to(:stats) }
-  it { should respond_to(:base_stats) }
+  let :zendark { Character.find_by_name('Zendark') || create(:zendark) }
+
+  it 'can list stats' do
+    expect(zendark).to respond_to :stats
+  end
+
+  it 'can list base stats' do
+    expect(zendark).to respond_to :base_stats
+  end
 
   context 'using stat_for' do
     it 'can get the base_stat value' do
-      expect(create(:zendark).base_stat_for :strength)
-          .to_eq 65
+      expect(zendark.base_stat_for :strength).to_eq 65
     end
 
     it 'can get the stat\'s value' do
-      expect(create(:zendark).stat_for :strength)
-          .to_eq 85
+      expect(zendark.stat_for :strength).to_eq 85
     end
   end
 
   context 'using base_stat_of' do
     it 'can get the base_stat value' do
-      expect(create(:zendark).base_stat_of :strength)
-          .to_eq 65
+      expect(zendark.base_stat_of :strength).to_eq 65
     end
 
     it 'can get the base stat\'s value' do
-      expect(create(:zendark).stat_of :strength)
-          .to_eq 85
+      expect(zendark.stat_of :strength).to_eq 85
     end
   end
 end
