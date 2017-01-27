31740fbb288e66b641e109a4addd99d94d44077a
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 22:46:52 +0100

    Rewrote career_spec to be cleaner.

--- spec/models/character_spec/inventory_spec.rb
+++ spec/models/character_spec/inventory_spec.rb
@@ -1,39 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Character Inventory', type: :model do
+  let(:zendark)     { Character.find_by_name('Zendark')  || create(:zendark) }
+  let(:candles)     { Thing.find_by_name('Candles')      || create(:candles) }
+  let(:burger_king) { Thing.find_by_name('Burger King')  || create(:burger_king) }
+  let(:big_mace)    { Weapon.find_by_name('Big Mace')    || create(:big_mace) }
+  let(:plaid_shirt) { Armour.find_by_name('Plaid Shirt') || create(:plaid_shirt) }
+
   it 'can be listed altogether' do
-    expect(create(:zendark).inventory)
-      .to eq([Weapon.find_by_name('Big Mace'), 
-              Armour.find_by_name('Plaid shirt'),
-              Thing.find_by_name('Candles')])
+    expect(zendark.inventory).to eq [big_mace, plaid_shirt, candles]
   end
 
   it 'can be listed as various inventories based on location' do
-    expect(create(:zendark)).to respond_to(:inventories)
+    expect(zendark).to respond_to :inventories
   end
 
   it 'can be gotten as a specific location\'s inventory' do
-    expect(create(:zendark).inventory(:bag))
-      .to eq([Thing.find_by_name('Candles')])
+    expect(zendark.inventory(:bag)).to eq [candles]
   end
 
   it 'can have some weapons' do
-    expect(create(:zendark).weapons).to eq([Weapon.find_by_name('Big Mace')])
+    expect(zendark.weapons).to eq [big_mace]
   end
 
   it 'can have armours' do
-    expect(create(:zendark).armours).to eq([Armour.find_by_name('Plaid shirt')])
+    expect(zendark.armours).to eq [plaid_shirt]
   end
 
   it 'can have random stuff' do
-    expect(create(:zendark).things).to eq([Thing.find_by_name('Candles')])
+    expect(zendark.things).to eq [candles]
   end
 
   it 'can be added new items' do
-    zendark = create(:zendark)
-    zendark.add_to_inventory(:bag, create(:burger_king))
-    expect(zendark.inventory(:bag))
-      .to eq([Thing.find_by_name('Candles'),
-              Thing.find_by_name('Burger King')])
+    zendark.add_to_inventory :bag, burger_king
+    expect(zendark.inventory(:bag)).to eq [candles, burger_king]
   end
 end
