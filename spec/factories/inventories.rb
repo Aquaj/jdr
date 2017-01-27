5ddb404ff0337bca4a91258d4bc5743b693d87eb
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-27 21:43:34 +0100

    Inventory factory.

--- spec/factories/inventories.rb
+++ spec/factories/inventories.rb
@@ -1,5 +1,18 @@
 FactoryGirl.define do
-  factory :inventory do
-    
+  factory :bag, class: :inventory do
+    location :bag
+
+    after do |bag|
+      create(:inventory_change, item_id: create(:candles), inventory_id: bag)
+    end
+  end
+
+  factory :horsebags, class: :inventory do
+    location :horsebags
+
+    after do |horsebags|
+      create(:inventory_change, item_id: create(:plaid_shirt), inventory_id: horsebags)
+      create(:inventory_change, item_id: create(:big_mace), inventory_id: horsebags)
+    end
   end
 end
