a3bd684e7eaed037be8cfa2c0313d48113c01dd8
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-28 12:21:33 +0100

    Better syntax for association.

--- spec/factories/base_stats.rb
+++ spec/factories/base_stats.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :base_stat do
     factory :base_strength do
-      association :stat, factory: :strength
+      stat factory: :strength
       value 65
     end
 
     factory :base_agility do
-      association :stat, factory: :agility
+      stat factory: :agility
       value 40
     end
   end
