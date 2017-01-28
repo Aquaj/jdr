f82c8617a9ff9f3d525a67e1efea76d3fb99c2fa
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-28 12:22:35 +0100

    Replacing creates by find_or_creates.

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -17,9 +17,15 @@ FactoryGirl.define do
       notable_details 'Scar'
 
 
-      after(:create) do |zendark|
-        create(:base_strength, character_id: zendark)
-        create(:base_agility, character_id: zendark)
+      after(:find_or_create) do |zendark|
+        find_or_create(:base_strength, character_id: zendark)
+        find_or_create(:base_agility, character_id: zendark)
+        
+        find_or_create(:bag, character_id: zendark)
+        find_or_create(:horsebags, character_id: zendark)
+
+        find_or_create(:career_change, character_id: zendark, from_id: nil, to_id: find_or_create(:diva))
+        find_or_create(:career_change, character_id: zendark, from_id: nil, to_id: find_or_create(:farm_bull))
       end
     end
   end
--- spec/factories/inventories.rb
+++ spec/factories/inventories.rb
@@ -3,17 +3,17 @@ FactoryGirl.define do
     factory :bag do
       location :bag
 
-      after do |bag|
-        create(:inventory_change, item_id: create(:candles), inventory_id: bag)
+      after(:find_or_create) do |bag|
+        find_or_create(:inventory_change, item_id: find_or_create(:candles), inventory_id: bag)
       end
     end
 
     factory :horsebags do
       location :horsebags
 
-      after do |horsebags|
-        create(:inventory_change, item_id: create(:plaid_shirt), inventory_id: horsebags)
-        create(:inventory_change, item_id: create(:big_mace), inventory_id: horsebags)
+      after(:find_or_create) do |horsebags|
+        find_or_create(:inventory_change, item_id: find_or_create(:plaid_shirt), inventory_id: horsebags)
+        find_or_create(:inventory_change, item_id: find_or_create(:big_mace), inventory_id: horsebags)
       end
     end
   end
