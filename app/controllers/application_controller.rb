5fc927f76d8c866b42d6d7b66ccbaef1ebc451bd
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-29 18:09:11 +0100

    Added authenticate_user! hook in AppController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+
+  before_action :authenticate_user!
 end
