e2b43c884c77dbdce548ce8e19859cdfbe300310
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-29 19:26:01 +0100

    Added nature attribute to stats.

--- db/migrate/20170122131847_create_stats.rb
+++ db/migrate/20170122131847_create_stats.rb
@@ -2,6 +2,7 @@ class CreateStats < ActiveRecord::Migration[5.0]
   def change
     create_table :stats do |t|
       t.string :name
+      t.string :nature
 
       t.timestamps
     end
--- spec/factories/stats.rb
+++ spec/factories/stats.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :stat do
     factory :strength do
       name :strength
+      nature :primary
     end
 
     factory :agility do
       name :agility
+      nature :primary
     end
   end
 end
