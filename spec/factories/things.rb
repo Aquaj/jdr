f00227657ad47de8bcc1aebce3963c5d0b041303
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-29 19:41:03 +0100

    Changed quantity to use integer in thing factories

--- spec/factories/things.rb
+++ spec/factories/things.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :thing do
     factory :candles do
-      name          'Candles'  
+      name          'Candles'
       unit          ''
       details       'Scented'
-      quantity      '5'
+      quantity      5
       enc_per_unit  1
     end
 
     factory :burger_king do
-      name          'Burger King'  
+      name          'Burger King'
       unit          'bks'
       details       'Wrapped'
-      quantity      '15'
+      quantity      15
       enc_per_unit  1
     end
   end
