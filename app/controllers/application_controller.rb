610d71e586d2094902e2075b989c84531ffa48fe
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-29 14:41:53 +0100

    Removed useless after_sign_in for a better root route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,4 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    characters_path
-  end
-
 end
