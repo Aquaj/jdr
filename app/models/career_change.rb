26e62f14db3d8031e9d4e0b8a7f84ec0eece50ec
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-29 13:25:39 +0100

    Made from/to optional.

--- app/models/career_change.rb
+++ app/models/career_change.rb
@@ -1,5 +1,5 @@
 class CareerChange < ApplicationRecord
   belongs_to :character
-  belongs_to :from, class_name: 'Career'
-  belongs_to :to,   class_name: 'Career'
+  belongs_to :from, class_name: 'Career', optional: true
+  belongs_to :to,   class_name: 'Career', optional: true
 end
