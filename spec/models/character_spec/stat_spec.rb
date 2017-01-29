bf926acf69d573a105f93d3df629ad893c7d848d
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-29 18:10:00 +0100

    Cleaned up stat_spec.

--- spec/models/character_spec/stat_spec.rb
+++ spec/models/character_spec/stat_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Character Stats', type: :model do
-  let :zendark { find_or_create(:zendark) }
+  let :zendark  { find_or_create(:zendark)  }
+  let :strength { find_or_create(:strength) }
 
   it 'can list stats' do
     expect(zendark).to respond_to :stats
@@ -13,21 +14,21 @@ RSpec.describe 'Character Stats', type: :model do
 
   context 'using stat_for' do
     it 'can get the base_stat value' do
-      expect(zendark.base_stat_for :strength).to_eq 65
+      expect(zendark.base_stat_for strength).to eq 65
     end
 
     it 'can get the stat\'s value' do
-      expect(zendark.stat_for :strength).to_eq 85
+      expect(zendark.stat_for strength).to eq 85
     end
   end
 
   context 'using base_stat_of' do
     it 'can get the base_stat value' do
-      expect(zendark.base_stat_of :strength).to_eq 65
+      expect(zendark.base_stat_of strength).to eq 65
     end
 
     it 'can get the base stat\'s value' do
-      expect(zendark.stat_of :strength).to_eq 85
+      expect(zendark.stat_of strength).to eq 85
     end
   end
 end
