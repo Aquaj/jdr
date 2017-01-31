c792093ba04a5d1405862c92d574d4612352d40c
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-01-31 12:41:36 +0100

    Removed useless aliases + extended the system to handle base_stats too.

--- app/models/has/stats.rb
+++ app/models/has/stats.rb
@@ -11,23 +11,32 @@ module Has
       base_stats.find_by(stat: stat)
                &.value
     end
-    alias base_stat_for base_stat_of
 
     def stat_of stat
       base    = base_stats.find_by stat: stat
       changes = base.stat_changes.pluck :amount
       base.value + changes.sum
     end
-    alias stat_for stat_of
 
     def method_missing method_name, *args, &block
-      stat = Stat[method_name]
+      stat   = Stat[method_name]
       return stat_of stat if stat
+
+      stat_name = extract_base_from method_name
+      stat      = Stat[stat_name]
+      return base_stat_of stat if stat
       super
     end
 
     def respond_to_missing? method_name, _include_private = false
-      Stat[method_name]
+      return true if Stat[method_name]
+      stat_name = extract_base_from method_name
+      return true if Stat[stat_name]
+    end
+
+    def extract_base_from name
+      match_base = name.to_s.match %r(base_(.*))
+      match_base && match_base[1].to_sym
     end
   end
 end
