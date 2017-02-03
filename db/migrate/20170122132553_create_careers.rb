7746e7ff58547c02005d14c474e359ca9405a260
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-02-03 20:58:16 +0100

    Added a 'secret' bool column in careers.

--- db/migrate/20170122132553_create_careers.rb
+++ db/migrate/20170122132553_create_careers.rb
@@ -1,8 +1,9 @@
 class CreateCareers < ActiveRecord::Migration[5.0]
   def change
     create_table :careers do |t|
-      t.string     :name
-      t.string     :nature
+      t.string  :name
+      t.string  :nature
+      t.boolean :secret
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20170124190642) do
   create_table "careers", force: :cascade do |t|
     t.string   "name"
     t.string   "nature"
+    t.boolean  "secret"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
