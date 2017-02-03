560fb3b20456bc0792302f8515fc674c4b1132c7
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-02-03 20:59:30 +0100

    Wrote spec for few career methods

--- spec/models/career_spec.rb
+++ spec/models/career_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Career, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let :diva   { find_or_create :diva          }
+  let :icon   { find_or_create :icon          }
+  let :secret { find_or_create :secret_career }
+
+  it 'lists its exits' do
+    expect(diva.exits).to contain_exactly icon, secret
+  end
+
+  it 'says if it\'s a secret classes or not' do
+    expect(secret).to   be_secret
+    expect(diva).not_to be_secret
+  end
+
+  it 'says if it\'s an advanced career of not' do
+    expect(diva).to be_basic
+    expect(icon).to be_advanced
+
+    expect(icon).not_to be_basic
+    expect(diva).not_to be_advanced
+  end
 end
