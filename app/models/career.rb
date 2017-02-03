546454df7118b40d4666fe77ff0e1b207109af78
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-02-03 21:01:23 +0100

    Implemented the career_spec

--- app/models/career.rb
+++ app/models/career.rb
@@ -2,11 +2,23 @@ class Career < ApplicationRecord
   has_many :stat_bonuses
   has_many :skill_choices
 
-  has_many :ascendant_parentages,  class_name: 'Parentage', foreign_key: :leads_to
-  has_many :descendant_parentages, class_name: 'Parentage', foreign_key: :source_career
+  has_many :ascendant_parentages,  class_name: 'Parentage', foreign_key: :leads_to_id
+  has_many :descendant_parentages, class_name: 'Parentage', foreign_key: :source_career_id
 
-  has_many :ascendants,  through: :ascendant_parentages,  class_name: 'Career'
-  has_many :descendants, through: :descendant_parentages, class_name: 'Career'
+  has_many :entries,  through: :ascendant_parentages,  source: :source_career, class_name: 'Career'
+  has_many :exits,    through: :descendant_parentages, source: :leads_to ,class_name: 'Career'
 
   has_many :awarenesses, as: :fact
+
+  def secret?
+    secret
+  end
+
+  def advanced?
+    nature == 'advanced'
+  end
+
+  def basic?
+    nature == 'basic'
+  end
 end
