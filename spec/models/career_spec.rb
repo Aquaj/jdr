73cf806a4384df3e86040c27d4b07362870acd72
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-02-04 17:16:06 +0100

    Fixed small errors in career_spec.

--- spec/models/career_spec.rb
+++ spec/models/career_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Career, type: :model do
-  let :diva   { find_or_create :diva          }
-  let :icon   { find_or_create :icon          }
-  let :secret { find_or_create :secret_career }
+  let :diva       { find_or_create :diva          }
+  let :icon       { find_or_create :icon          }
+  let :secret     { find_or_create :secret_career }
+  let :gay        { find_or_create :gay           }
+  let :fabulous   { find_or_create :fabulous      }
+  let :dancing    { find_or_create :dancing       }
+  let :sassy_snap { find_or_create :sassy_snap    }
 
   it 'lists its exits' do
     expect(diva.exits).to contain_exactly icon, secret
@@ -23,17 +27,17 @@ RSpec.describe Career, type: :model do
   end
 
   it 'lists its abilities' do
-    expect(diva.skills).to
-      contain_exactly dancing, sassy_snap
+    expect(diva.abilities)
+      .to contain_exactly dancing, sassy_snap
   end
 
   it 'lists its talents' do
-    expect(diva.skills).to
-      contain_exactly fabulous
+    expect(diva.talents)
+      .to contain_exactly fabulous, gay
   end
 
   it 'lists its skills' do
-    expect(diva.skills).to
-      contain_exactly dancing, sassy_snap, fabulous
+    expect(diva.skills)
+      .to contain_exactly dancing, sassy_snap, fabulous, gay
   end
 end
