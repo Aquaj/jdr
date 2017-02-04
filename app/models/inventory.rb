d2213370dd26da3ee59a1deb72e5968aa348bf5f
Jeremie Bonal <jeremie.bonal@gmail.com>
2017-02-04 15:02:38 +0100

    Added missing relations.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -4,6 +4,12 @@ class Inventory < ApplicationRecord
   belongs_to :owner, polymorphic: true, optional: true
   has_many   :inventory_changes
 
+  with_options through: :inventory_changes, source: :item do
+    has_many   :things,  source_type: 'Thing'
+    has_many   :armours, source_type: 'Armour'
+    has_many   :weapons, source_type: 'Weapon'
+  end
+
   def add_items stuff
     stuff.each do |item, amount|
       inventory_changes.create! item: item, amount: amount
